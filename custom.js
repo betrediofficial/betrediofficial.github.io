b4fe4378ed46ed0eb23ebe06b427c3807bb0483b
betrediofficial <betredi90@gmail.com>
2024-12-20 00:54:18 +0300

    sd

--- custom.js
+++ custom.js
@@ -17,18 +17,26 @@ function initialize(){
 
 function bottomMenuWidget(){
     $(".section:first").append(`
-        <div style="display: flex; flex-wrap: wrap; justify-content: center; gap: 10px; max-width: 1200px; margin: 0 auto; padding: 10px;">
+<div style="display: flex; flex-wrap: wrap; justify-content: center; gap: 10px; max-width: 1200px; margin: 0 auto; padding: 10px;">
   <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
-     <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/games.png" alt="games" border="0"></a><br />
+    <a href="https://imgbb.com/">
+      <img src="https://betrediofficial.github.io/images/games.png" alt="games" style="width: 100%; max-width: 200px; height: auto;">
+    </a>
   </div>
   <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
-     <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/maconcesi.png" alt="maconcesi" border="0"></a><br />
+    <a href="https://imgbb.com/">
+      <img src="https://betrediofficial.github.io/images/maconcesi.png" alt="maconcesi" style="width: 100%; max-width: 200px; height: auto;">
+    </a>
   </div>
   <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
-     <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/sporb.png" alt="sporb" border="0"></a><br />
+    <a href="https://imgbb.com/">
+      <img src="https://betrediofficial.github.io/images/sporb.png" alt="sporb" style="width: 100%; max-width: 200px; height: auto;">
+    </a>
   </div>
   <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
-    <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/slotcasino.png" alt="slotcasino" border="0"></a><br />
+    <a href="https://imgbb.com/">
+      <img src="https://betrediofficial.github.io/images/slotcasino.png" alt="slotcasino" style="width: 100%; max-width: 200px; height: auto;">
+    </a>
   </div>
 </div>
     `);
