b04a8d92bdf5615e20413010126a29a255c6a53f
betrediofficial <betredi90@gmail.com>
2024-12-20 00:12:41 +0300

    sdsd

--- custom.js
+++ custom.js
@@ -3,7 +3,7 @@ if (typeof jQuery === 'undefined') {
     script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
     script.onload = function() {
         $( document ).ready(function() {
-            alert('ready')
+          initialize()
         });
     };
     document.head.appendChild(script);
@@ -11,3 +11,27 @@ if (typeof jQuery === 'undefined') {
     console.log('jQuery zaten mevcut.');
 }
 
+function initialize(){
+    bottomMenuWidget();
+}
+
+function bottomMenuWidget(){
+    $(".section:first").append(`
+        <div class="container-xxl">
+          <div class="row align-items-center text-center">
+            <div class="col">
+              <a href="https://imgbb.com/"><img src="https://i.ibb.co/zhhX3v5/games.png" alt="games" border="0"></a><br />
+            </div>
+            <div class="col">
+              <a href="https://imgbb.com/"><img src="https://i.ibb.co/hx04FPP/maconcesi.png" alt="maconcesi" border="0"></a><br />
+            </div>
+            <div class="col">
+              <a href="https://imgbb.com/"><img src="https://i.ibb.co/Zfhkq2H/sporb.png" alt="sporb" border="0"></a><br />
+            </div>
+            <div class="col">
+              <a href="https://imgbb.com/"><img src="https://i.ibb.co/R6P7XP8/slotcasino.png" alt="slotcasino" border="0"></a><br />
+            </div>
+          </div>
+        </div>
+    `);
+}
