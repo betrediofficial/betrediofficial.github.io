1d301472d1aa89877b9575f8dbfd33c2c57d255b
betrediofficial <betredi90@gmail.com>
2024-12-20 00:18:22 +0300

    images

--- custom.js
+++ custom.js
@@ -20,16 +20,16 @@ function bottomMenuWidget(){
         <div class="container-xxl">
           <div class="row align-items-center text-center">
             <div class="col">
-              <a href="https://imgbb.com/"><img src="https://i.ibb.co/zhhX3v5/games.png" alt="games" border="0"></a><br />
+              <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/games.png" alt="games" border="0"></a><br />
             </div>
             <div class="col">
-              <a href="https://imgbb.com/"><img src="https://i.ibb.co/hx04FPP/maconcesi.png" alt="maconcesi" border="0"></a><br />
+              <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/maconcesi.png" alt="maconcesi" border="0"></a><br />
             </div>
             <div class="col">
-              <a href="https://imgbb.com/"><img src="https://i.ibb.co/Zfhkq2H/sporb.png" alt="sporb" border="0"></a><br />
+              <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/sporb.png" alt="sporb" border="0"></a><br />
             </div>
             <div class="col">
-              <a href="https://imgbb.com/"><img src="https://i.ibb.co/R6P7XP8/slotcasino.png" alt="slotcasino" border="0"></a><br />
+              <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/slotcasino.png" alt="slotcasino" border="0"></a><br />
             </div>
           </div>
         </div>
