7fb09d583287affba92374d5d76d5340d6423b87
betrediofficial <betredi90@gmail.com>
2024-12-20 00:57:08 +0300

    fix

--- custom.js
+++ custom.js
@@ -20,22 +20,22 @@ function bottomMenuWidget(){
 <div style="display: flex; flex-wrap: wrap; justify-content: center; gap: 10px; max-width: 1200px; margin: 0 auto; padding: 10px;">
   <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
     <a href="https://imgbb.com/">
-      <img src="https://betrediofficial.github.io/images/games.png" alt="games" style="width: 100%; max-width: 200px; height: auto;">
+      <img src="https://betrediofficial.github.io/images/games.png" alt="games" style="width: 100%; height: auto;">
     </a>
   </div>
   <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
     <a href="https://imgbb.com/">
-      <img src="https://betrediofficial.github.io/images/maconcesi.png" alt="maconcesi" style="width: 100%; max-width: 200px; height: auto;">
+      <img src="https://betrediofficial.github.io/images/maconcesi.png" alt="maconcesi" style="width: 100%; height: auto;">
     </a>
   </div>
   <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
     <a href="https://imgbb.com/">
-      <img src="https://betrediofficial.github.io/images/sporb.png" alt="sporb" style="width: 100%; max-width: 200px; height: auto;">
+      <img src="https://betrediofficial.github.io/images/sporb.png" alt="sporb" style="width: 100%; height: auto;">
     </a>
   </div>
   <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
     <a href="https://imgbb.com/">
-      <img src="https://betrediofficial.github.io/images/slotcasino.png" alt="slotcasino" style="width: 100%; max-width: 200px; height: auto;">
+      <img src="https://betrediofficial.github.io/images/slotcasino.png" alt="slotcasino" style="width: 100%; height: auto;">
     </a>
   </div>
 </div>
