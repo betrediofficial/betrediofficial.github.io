ee2c505bdf64958b62bf4a0af4cc18bc10a63bd8
betrediofficial <betredi90@gmail.com>
2024-12-20 00:42:50 +0300

    wewe

--- custom.js
+++ custom.js
@@ -17,21 +17,19 @@ function initialize(){
 
 function bottomMenuWidget(){
     $(".section:first").append(`
-        <div class="container-xxl">
-          <div class="row align-items-center text-center">
-            <div class="col">
-              <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/games.png" alt="games" border="0"></a><br />
-            </div>
-            <div class="col">
-              <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/maconcesi.png" alt="maconcesi" border="0"></a><br />
-            </div>
-            <div class="col">
-              <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/sporb.png" alt="sporb" border="0"></a><br />
-            </div>
-            <div class="col">
-              <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/slotcasino.png" alt="slotcasino" border="0"></a><br />
-            </div>
-          </div>
-        </div>
+        <div style="display: flex; flex-wrap: wrap; justify-content: center; gap: 10px; max-width: 1200px; margin: 0 auto; padding: 10px;">
+  <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
+     <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/games.png" alt="games" border="0"></a><br />
+  </div>
+  <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
+     <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/maconcesi.png" alt="maconcesi" border="0"></a><br />
+  </div>
+  <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
+     <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/sporb.png" alt="sporb" border="0"></a><br />
+  </div>
+  <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
+    <a href="https://imgbb.com/"><img src="https://betrediofficial.github.io/images/slotcasino.png" alt="slotcasino" border="0"></a><br />
+  </div>
+</div>
     `);
 }
