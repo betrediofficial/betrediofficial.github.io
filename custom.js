ebcac5f28f62352839e9e7910bc7acf578aee3b0
betrediofficial <betredi90@gmail.com>
2024-12-20 21:53:39 +0300

    asdasd

--- custom.js
+++ custom.js
@@ -7,7 +7,7 @@ try{
     script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
     script.onload = function() {
         $( document ).ready(function() {
-          // initialize()
+          initialize()
         });
     };
     document.head.appendChild(script);
