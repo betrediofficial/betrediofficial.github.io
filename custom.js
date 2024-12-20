1d11c9b94406874b1846eceba1f230c853338200
betrediofficial <betredi90@gmail.com>
2024-12-20 18:30:42 +0300

    sdsd

--- custom.js
+++ custom.js
@@ -21,34 +21,56 @@ function otherGames(){
 
         var newSection = `
            
-    <div class="section id="casinooyunlari">
-  <div class="otherGames">
-        <h1 class="otherGamesHeader">CANLI CASINO OYUNLARI</h1>
+<div class="section" id="casinooyunlari">
+  <div class="container otherGames">
+    <div class="row">
+      <div class="col-12">
+        <div class="section__title-wrap"><h2 class="section__title"><svg class="svg-icon"><use href="/static/media/sprite.33143bc5180d8cec7f771b3eedc5187c.svg#top-games"></use></svg>En Popüler Oyunlar</h2><a class="section__view section__view--carousel" href="/tr/casino/group/top-games">Tümünü Görüntüle</a></div>
+      </div>
+      <div class="col-12">
+        <div class="otherGamesComponent">
+          <div class="otherGamesComponentLayout">
+            <a href="https://imgbb.com/">
+              <img
+                src="https://betrediofficial.github.io/images/other-games/aviator.png"
+                alt="games"
+                class="otherGamesComponentLayoutImg"
+              />
+            </a>
+          </div>
+          <div class="otherGamesComponentLayout">
+            <a href="https://imgbb.com/">
+              <img
+                src="https://betrediofficial.github.io/images/other-games/spaceman.png"
+                alt="maconcesi"
+                class="otherGamesComponentLayoutImg"
+              />
+            </a>
+          </div>
+          <div class="otherGamesComponentLayout">
+            <a href="https://imgbb.com/">
+              <img
+                src="https://betrediofficial.github.io/images/other-games/zepplin.png"
+                alt="sporb"
+                class="otherGamesComponentLayoutImg"
+              />
+            </a>
+          </div>
+          <div class="otherGamesComponentLayout">
+            <a href="https://imgbb.com/">
+              <img
+                src="https://betrediofficial.github.io/images/other-games/bigbass.png"
+                alt="slotcasino"
+                class="otherGamesComponentLayoutImg"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-  <div class="otherGamesComponent">
-  <div class="otherGamesComponentLayout">
-    <a href="https://imgbb.com/">
-      <img src="https://betrediofficial.github.io/images/other-games/aviator.png" alt="games" class="otherGamesComponentLayoutImg">
-    </a>
-  </div>
-  <div class="otherGamesComponentLayout">
-    <a href="https://imgbb.com/">
-      <img src="https://betrediofficial.github.io/images/other-games/spaceman.png" alt="maconcesi" class="otherGamesComponentLayoutImg">
-    </a>
-  </div>
-  <div class="otherGamesComponentLayout">
-    <a href="https://imgbb.com/">
-      <img src="https://betrediofficial.github.io/images/other-games/zepplin.png" alt="sporb" class="otherGamesComponentLayoutImg">
-    </a>
-  </div>
-  <div class="otherGamesComponentLayout">
-    <a href="https://imgbb.com/">
-      <img src="https://betrediofficial.github.io/images/other-games/bigbass.png" alt="slotcasino" class="otherGamesComponentLayoutImg">
-    </a>
-  </div>
-</div>
-</div>
-</div>
         `;
 
         $('.section--first')[1].before(newSection)
