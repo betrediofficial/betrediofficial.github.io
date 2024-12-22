bd8ac546e48e0df363247aeb908abeb746852319
betrediofficial <betredi90@gmail.com>
2024-12-22 23:05:31 +0200

    sd

--- custom.js
+++ custom.js
@@ -244,14 +244,21 @@ try {
       <img src="https://betrediofficial.github.io/images/games.png" alt="games" class="bottomMenuWidgetImage" >
     </a>
   </div>
-  <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
+  
+  <!-- <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
     <a href="https://imgbb.com/">
       <img src="https://betrediofficial.github.io/images/maconcesi.png" alt="maconcesi" class="bottomMenuWidgetImage" >
+    </a>
+  </div> -->
+
+  <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
+    <a href="https://imgbb.com/">
+      <img src="https://betrediofficial.github.io/images/sporb.png" alt="sporb" class="bottomMenuWidgetImage" >
     </a>
   </div>
   <div style="flex: 1 1 calc(25% - 10px); text-align: center;">
     <a href="https://imgbb.com/">
-      <img src="https://betrediofficial.github.io/images/sporb.png" alt="sporb" class="bottomMenuWidgetImage" >
+      <img src="https://betrediofficial.github.io/images/slotcasino.png" alt="slotcasino" class="bottomMenuWidgetImage" >
     </a>
   </div>
 </div>
