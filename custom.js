b1bb274f6b710df9648d7a4262abcd06c80de94a
betrediofficial <betredi90@gmail.com>
2025-03-11 18:31:48 +0300

    s

--- custom.js
+++ custom.js
@@ -107,7 +107,7 @@ try {
       <div class="col-12">
         <div class="section__title-wrap"><h2 class="section__title"><svg class="svg-icon"><use href="/static/media/sprite.33143bc5180d8cec7f771b3eedc5187c.svg#top-games"></use></svg>
         EKSTRA OYUNLAR
-        </h2><a class="section__view section__view--carousel" href="/tr/casino/group/top-games">Tümünü Görüntüle</a></div>
+        </h2><a class="section__view section__view--carousel" href="/casino/group/top-games">Tümünü Görüntüle</a></div>
       </div>
       <div class="col-12">
         <div class="otherGamesComponent">
