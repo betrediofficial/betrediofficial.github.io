74709c3b9f860e63db4231242fdeda89897cbca3
betrediofficial <betredi90@gmail.com>
2025-03-27 22:52:05 +0200

    upd

--- custom-dev.js
+++ custom-dev.js
@@ -275,7 +275,7 @@
     <div class="row">
       <div class="col-12">
         <div class="section__title-wrap">
-          <h2 class="section__title" style="font-size: 24px;">
+          <h2 class="section__title" style="font-size: 30px;">
             SLOT CASINO
           </h2>
         </div>
