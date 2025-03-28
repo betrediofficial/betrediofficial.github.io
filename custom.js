e1de9111da51968b294903b88b244cca8b85a51f
betrediofficial <betredi90@gmail.com>
2025-03-28 05:04:22 +0200

    sports card on homepage v2

--- custom.js
+++ custom.js
@@ -576,7 +576,7 @@
 <div class="container">
   <div class="manually-added-home-widgets section" id="sportscard" style="margin-bottom: 48px;">
     <div class="container otherGames">
-      <div class="row row-cols-2 row-cols-md-3 row-cols-lg-6 g-3 justify-content-center text-center">
+      <div class="row row-cols-2 row-cols-md-3 g-3 justify-content-center text-center">
 
         <div class="col">
         <a href="/sportsbook">
