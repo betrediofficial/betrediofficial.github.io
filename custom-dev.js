66bb608ea5f602efeae6196559412b58ad504e80
betrediofficial <betredi90@gmail.com>
2025-03-29 10:24:26 +0200

    qwe v14

--- custom-dev.js
+++ custom-dev.js
@@ -87,78 +87,78 @@
 
       // if (language !== "tr") return;
 
-      // function customizeSignupModal() {
-      //   const imgUrl =
-      //     "https://betrediofficial.github.io/images/signup-banner/betredi_banner.png";
-
-      //   const observer = new MutationObserver(() => {
-      //     const $modal = $("#signup-modal");
-      //     const $content = $modal.find(".modal__content").last();
-
-      //     if (
-      //       $modal.is(":visible") &&
-      //       $content.length &&
-      //       $content.find(".modal__sign-img").length === 0
-      //     ) {
-      //       const $signImg = $(`
-      //   <div class="modal__sign-img">
-      //     <img src="${imgUrl}" alt="Betredi Banner" />
-      //   </div>
-      // `);
-
-      //       $content.prepend($signImg);
-      //     }
-      //   });
-
-      //   observer.observe(document.body, {
-      //     childList: true,
-      //     subtree: true,
-      //   });
-      // }
-
-      function waitForElement(
-        selector,
-        callback,
-        interval = 50,
-        timeout = 2000
-      ) {
-        const start = Date.now();
-        const timer = setInterval(() => {
-          const $el = $(selector);
-          if ($el.length) {
-            clearInterval(timer);
-            callback($el);
-          } else if (Date.now() - start > timeout) {
-            clearInterval(timer);
-            console.warn(`Element ${selector} not found in time.`);
-          }
-        }, interval);
-      }
-
       function customizeSignupModal() {
         const imgUrl =
           "https://betrediofficial.github.io/images/signup-banner/betredi_banner.png";
 
-        $("#signup-modal").on("shown.bs.modal", function () {
-          const $modal = $(this);
+        const observer = new MutationObserver(() => {
+          const $modal = $("#signup-modal");
+          const $content = $modal.find(".modal__content").last();
 
-          waitForElement("#signup-modal .modal__content", ($content) => {
-            if ($content.find(".modal__sign-img").length === 0) {
-              const signImgHtml = `
-          <div class="modal__sign-img">
-            <img src="${imgUrl}" alt="Betredi Banner" />
-          </div>
-        `;
-              $content.prepend(signImgHtml);
-              console.log("✅ .modal__sign-img injected!");
-            }
-          });
+          if (
+            $modal.is(":visible") &&
+            $content.length &&
+            $content.find(".modal__sign-img").length === 0
+          ) {
+            const $signImg = $(`
+        <div class="modal__sign-img">
+          <img src="${imgUrl}" alt="Betredi Banner" />
+        </div>
+      `);
+
+            $content.prepend($signImg);
+          }
         });
 
-        $("#signup-modal").on("hidden.bs.modal", function () {
-          $(this).find(".modal__sign-img").remove();
+        observer.observe(document.body, {
+          childList: true,
+          subtree: true,
         });
       }
+
+      // function waitForElement(
+      //   selector,
+      //   callback,
+      //   interval = 50,
+      //   timeout = 2000
+      // ) {
+      //   const start = Date.now();
+      //   const timer = setInterval(() => {
+      //     const $el = $(selector);
+      //     if ($el.length) {
+      //       clearInterval(timer);
+      //       callback($el);
+      //     } else if (Date.now() - start > timeout) {
+      //       clearInterval(timer);
+      //       console.warn(`Element ${selector} not found in time.`);
+      //     }
+      //   }, interval);
+      // }
+
+      // function customizeSignupModal() {
+      //   const imgUrl =
+      //     "https://betrediofficial.github.io/images/signup-banner/betredi_banner.png";
+
+      //   $("#signup-modal").on("shown.bs.modal", function () {
+      //     const $modal = $(this);
+
+      //     waitForElement("#signup-modal .modal__content", ($content) => {
+      //       if ($content.find(".modal__sign-img").length === 0) {
+      //         const signImgHtml = `
+      //     <div class="modal__sign-img">
+      //       <img src="${imgUrl}" alt="Betredi Banner" />
+      //     </div>
+      //   `;
+      //         $content.prepend(signImgHtml);
+      //         console.log("✅ .modal__sign-img injected!");
+      //       }
+      //     });
+      //   });
+
+      //   $("#signup-modal").on("hidden.bs.modal", function () {
+      //     $(this).find(".modal__sign-img").remove();
+      //   });
+      // }
 
       const is_mobile = isMobile();
 
