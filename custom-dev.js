59f30f519447771bc8bf88e4d79df84163ed0c8e
betrediofficial <betredi90@gmail.com>
2025-04-05 15:05:34 +0300

    qwewq

--- custom-dev.js
+++ custom-dev.js
@@ -881,37 +881,37 @@
 <div class="manually-added-home-widgets section" id="sportscard" style="margin-bottom: 10px;">
   <div class="container otherGames">
 <div class="row row-cols-3 row-cols-xl-6 g-3 justify-content-center text-center">
-      <div class="sport-card col" style="overflow: hidden !important; border: 2px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
+      <div class="sport-card col" style="overflow: hidden !important; border: 3px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
         <a href="/sportsbook">
           <img src="https://betrediofficial.github.io/images/sports/futbol_new.png" alt="Futbol" style="width: 100%;">
         </a>
       </div>
 
-      <div class="sport-card col" style="overflow: hidden !important; border: 2px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
+      <div class="sport-card col" style="overflow: hidden !important; border: 3px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
         <a href="/sportsbook">
           <img src="https://betrediofficial.github.io/images/sports/basketbol_new.png" alt="Basketbol" style="width: 100%;">
         </a>
       </div>
 
-      <div class="sport-card col" style="overflow: hidden !important; border: 2px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
+      <div class="sport-card col" style="overflow: hidden !important; border: 3px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
         <a href="/sportsbook">
           <img src="https://betrediofficial.github.io/images/sports/voleybol_new.png" alt="Voleybol" style="width: 100%;">
         </a>
       </div>
 
-      <div class="sport-card col" style="overflow: hidden !important; border: 2px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
+      <div class="sport-card col" style="overflow: hidden !important; border: 3px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
         <a href="/sportsbook">
           <img src="https://betrediofficial.github.io/images/sports/mma_new.png" alt="MMA" style="width: 100%;">
         </a>
       </div>
 
-      <div class="sport-card col" style="overflow: hidden !important; border: 2px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
+      <div class="sport-card col" style="overflow: hidden !important; border: 3px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
         <a href="/sportsbook">
           <img src="https://betrediofficial.github.io/images/sports/tennis_new.png" alt="Tenis" style="width: 100%;">
         </a>
       </div>
 
-      <div class="sport-card col" style="overflow: hidden !important; border: 2px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
+      <div class="sport-card col" style="overflow: hidden !important; border: 3px solid #9B000E; border-top-right-radius: 32px; border-bottom-left-radius: 32px;">
         <a href="/sportsbook">
           <img src="https://betrediofficial.github.io/images/sports/cycling_new.png" alt="Cycling" style="width: 100%;">
         </a>
