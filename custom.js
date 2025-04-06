0ee096a035cf3536df8c9d0b65479ae0f4086f17
betrediofficial <betredi90@gmail.com>
2025-04-07 00:34:57 +0300

    slider

--- custom.js
+++ custom.js
@@ -87,6 +87,26 @@
 
       // if (language !== "tr") return;
 
+      if (typeof Swiper !== "undefined") {
+        new Swiper("#main-slider-swiper", {
+          loop: true,
+          autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+          },
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          effect: "slide",
+          speed: 600,
+        });
+      }
+
       customCSS();
 
       function customizeSignupModal() {
