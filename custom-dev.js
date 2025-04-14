13806e82bb518c9b9ac8f0b2dee9f0fe3ee35521
betrediofficial <betredi90@gmail.com>
2025-04-14 17:09:26 +0300

    mini-games 2

--- custom-dev.js
+++ custom-dev.js
@@ -107,6 +107,11 @@
       swiperScript.src =
         "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js";
       document.head.appendChild(swiperScript);
+
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css";
+      document.head.appendChild(link);
     }
 
     // Wait for DOM and both libraries to be ready
