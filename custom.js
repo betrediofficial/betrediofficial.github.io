294ff4ab5e4f01566f85e45861ca48d7f5d9e856
betrediofficial <betredi90@gmail.com>
2025-04-17 21:56:11 +0300

    bottomMenuWidget converted section:first

--- custom.js
+++ custom.js
@@ -2465,7 +2465,7 @@
 
     function bottomMenuWidget(isMobile) {
       if ($("#bottomMenuWidgedContainer").length > 0) $("#bottomMenuWidgedContainer").remove();
-      $("#main-slider .container").after(`
+      $(".section:first").after(`
 <div class="manually-added-home-widgets bottomMenuWidgedContainer" id="bottomMenuWidgedContainer" style="margin-bottom: 10px;">
   <div class="bottom-menu-widget" style="flex: 1 1 calc(25% - 10px); text-align: center;">
     <a href="casino/category/exclusive">
