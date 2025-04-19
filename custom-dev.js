676349337169309564be9b90d3b70020b0202d10
betrediofficial <betredi90@gmail.com>
2025-04-19 19:00:11 +0300

    game chooser hover effect disabled when slotting

--- custom-dev.js
+++ custom-dev.js
@@ -1004,7 +1004,7 @@
 
         setTimeout(() => {
           $(this).prop("disabled", false);
-          // $(".game-chooser-hovered-effect").css("opacity", 0);
+          $(".game-chooser-hovered-effect").hide(); // veya .css("display", "none")
         }, 2000);
       });
 
@@ -2504,10 +2504,6 @@ ${
 
     #main-slider .swiper-slide a {
       height: 100% !important;
-    }
-
-    .disabled-game-chooser {
-      display: none !important;
     }
   }
       `;
