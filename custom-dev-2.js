585f0c8a90fb798d383fad56d29b34c00d3bca91
betrediofficial <betredi90@gmail.com>
2025-04-20 15:16:40 +0300

    otherGames

--- custom-dev-2.js
+++ custom-dev-2.js
@@ -2528,7 +2528,7 @@ ${
     // }
 
     function otherGames() {
-      alert('triggered unmounted')
+      alert($("#digeroyunlari").length)
       if ($("#digeroyunlari").length > 0) return $("#digeroyunlari").remove();
       alert('triggered')
       var newSection = `
